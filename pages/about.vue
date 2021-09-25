4780bf9b9d73a7155f9cb35b89d4e2793782ac71
lucadonnoh <donnoh99@gmail.com>
2021-09-25 18:19:29 +0200

    testing vue

--- pages/about.vue
+++ pages/about.vue
@@ -1,6 +1,29 @@
 <template>
-    <div class="container">
-        <h1>About</h1>
-        <p>This is the about page</p>
-    </div>
+  <c-box class="container" p="4">
+    <c-heading>About</c-heading>
+    <c-text>This is the about page!!</c-text>
+    <c-input placeholder="your input" v-model="inputValue" />
+    <c-text>This is your input: {{ inputValue }}</c-text>
+    <hr>
+    <c-box>
+        <c-button variant-color="blue" @click="toggleVisibility">toggle visibility</c-button>
+        <c-text v-if="isVisible">i am visible now!</c-text>
+    </c-box>
+  </c-box>
 </template>
+
+<script>
+    export default {
+        data() {
+            return {
+            inputValue: "",
+            isVisible: true,
+            };
+        },
+        methods: {
+            toggleVisibility() {
+                this.isVisible = !this.isVisible;
+            }
+        }
+    };
+</script>
