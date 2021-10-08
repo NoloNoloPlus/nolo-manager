2c88ce64d6539d90057a42be4adedec1e9274df4
lucadonnoh <donnoh99@gmail.com>
2021-10-08 19:31:43 +0200

    improved header

--- components/Header.vue
+++ components/Header.vue
@@ -1,7 +1,43 @@
 <template>
     <c-flex justify="space-around" align="center" h="3em">
-        <NuxtLink to="/"><c-heading size="md">Home</c-heading></NuxtLink>
-        <NuxtLink to="/clients"><c-heading size="md">Clients</c-heading></NuxtLink>
-        <NuxtLink to="/products"><c-heading size="md">Product</c-heading></NuxtLink>
+        <NuxtLink to="/">
+            <c-heading size="md">Home</c-heading>
+        </NuxtLink>
+        <c-menu>
+            <c-menu-button right-icon="chevron-down" variant-color="blue">
+                <c-heading size="md">Clients</c-heading>
+            </c-menu-button>
+            <c-menu-list>
+                <NuxtLink to="/clients">
+                    <c-menu-item>
+                        <c-heading size="sm">All clients</c-heading>
+                    </c-menu-item>
+                </NuxtLink>
+                <NuxtLink to="/clients/add">
+                    <c-menu-item>
+                        <c-heading size="sm">Add client</c-heading>
+                    </c-menu-item>
+                </NuxtLink>
+            </c-menu-list>
+        </c-menu>
+        <c-menu>
+            <c-menu-button right-icon="chevron-down" variant-color="red">
+                <c-heading size="md">Products</c-heading>
+            </c-menu-button>
+            <c-menu-list>
+                <NuxtLink to="/products">
+                    <c-menu-item>
+                        <c-heading size="sm">All products</c-heading>
+                    </c-menu-item>
+                </NuxtLink>
+                <NuxtLink to="/products/add">
+                    <c-menu-item>
+                        <c-heading size="sm">Add product</c-heading>
+                    </c-menu-item>
+                </NuxtLink>
+            </c-menu-list>
+        </c-menu>
+
+
     </c-flex>
 </template>
